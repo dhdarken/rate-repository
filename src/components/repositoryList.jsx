af515336c5307ed6af0383ac5e49b1f66c5a76e9
julian <juliansanvill@hotmail.com>
2023-09-22 17:57:35 -0500

    fixed ip condig in fetch

--- src/components/repositoryList.jsx
+++ src/components/repositoryList.jsx
@@ -1,27 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Text, FlatList } from "react-native";
 import RepositoryItem from "./RespositoryItem";
+import useRepositories from "../hooks/useRepositories";
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState(null);
-
-  const fetchRepositories = async () => {
-    const response = await globalThis.fetch(
-      "http://localhost:5000/api/repositories"
-    );
-    const json = await response.json();
-    setRepositories(json);
-  };
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-  const repositoriesNodes = repositories
-    ? repositories.edges?.map((edge) => edge.node)
-    : [];
-
+  const { repositories } = useRepositories();
   return (
     <FlatList
-      data={repositoriesNodes}
+      data={repositories}
       ItemSeparatorComponent={() => <Text></Text>}
       renderItem={({ item: repo }) => <RepositoryItem {...repo} />}
     />
