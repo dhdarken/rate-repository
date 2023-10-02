e58de26c08a47bfc33d9d9699d9438a6621b9899
julian <juliansanvill@hotmail.com>
2023-10-02 14:57:15 -0500

    add apollo and graphql

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Main from "./src/components/Main.jsx";
 import { NativeRouter } from "react-router-native";
 import { StatusBar } from "expo-status-bar";
+//import { ApolloProvider } from "@apollo/client";
+//import createApolloClient from "./src/utils/apolloClient.js";
+
+//const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-
       <NativeRouter>
         <Main></Main>
       </NativeRouter>
---
+++ src/graphql/queries.js
@@ -0,0 +1,2 @@
+import { gql } from "apollo-boost";
+export const GET_REPOSITORIES = gql``;
